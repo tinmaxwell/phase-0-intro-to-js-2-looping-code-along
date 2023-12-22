1376981505861dea8816615a9e928059012d7e5d
Max <tinmaxwell@gmail.com>
2023-12-21 19:31:04 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const cards = (['Guadalupe', 'Ollie', 'Aki'], "gift");
+
+function writeCards(cards) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < cards.length; i++) {
+      thankYouMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+
+  function countDown(number) {
+    if (number > 0) {
+      for (let i = number; i >= 0; i--) {
+        console.log(i);
+      }
+    } else {
+      console.log("Please provide a positive integer.");
+    }
+  }
+
